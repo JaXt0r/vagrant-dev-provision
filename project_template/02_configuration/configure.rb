6ec60199dfdefc59130c39264503ab64c1ae0df0
Michael Dähnert <michael.daehnert@gmail.com>
2017-08-01 10:47:07 +0200

    project template filled with demo data.

---
+++ project_template/02_configuration/configure.rb
@@ -0,0 +1,12 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+require 'yaml'
+require 'fileutils'
+require 'uri'
+
+
+def configure( &block )
+  p "Setup project-specific config".
+  p "Not implemented right now, because no use atm."
+  p "Will be developed when needed."
+end
---
+++ project_template/03_customization/customize.rb
@@ -0,0 +1,12 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+require 'yaml'
+require 'fileutils'
+require 'uri'
+
+
+def customize( &block )
+  p "Load user-specific folder. Name is taken from settings.user.yml - config".
+  p "Not implemented right now, because no use atm."
+  p "Will be developed when needed."
+end
